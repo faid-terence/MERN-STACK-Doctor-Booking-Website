4385733b842f1319611e53774c77c632be3d36b8
Faid Terence JABO <faidterence@outlook.com>
2023-07-29 18:27:22 +0200

    feature(footer):Component Implemneted

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,7 +1,117 @@
+import { Link } from "react-router-dom";
+import logo from "../../assets/images/logo.png";
+import { RiLinkedinFill } from "react-icons/ri";
+import {
+  AiFillYoutube,
+  AiFillGithub,
+  AiOutlineInstagram,
+} from "react-icons/ai";
 
+const socialLinks = [
+  {
+    path: "htttps://facebook.com",
+    icon: <AiFillYoutube className="group-hover:text-white w-4 h-5" />,
+  },
+  {
+    path: "htttps://facebook.com",
+    icon: <AiFillGithub className="group-hover:text-white w-4 h-5" />,
+  },
+  {
+    path: "htttps://facebook.com",
+    icon: <AiOutlineInstagram className="group-hover:text-white w-4 h-5" />,
+  },
+  {
+    path: "htttps://facebook.com",
+    icon: <RiLinkedinFill className="group-hover:text-white w-4 h-5" />,
+  },
+];
+const quickLinks01 = [
+  {
+    path: "/home",
+    display: "Home",
+  },
+  {
+    path: "/doctors",
+    display: "Find a Doctor",
+  },
+  {
+    path: "/services",
+    display: "Services",
+  },
+  {
+    path: "/contact",
+    display: "Contact Us",
+  },
+];
+const quickLinks02 = [
+  {
+    path: "/home",
+    display: "Home",
+  },
+  {
+    path: "/doctors",
+    display: "Find a Doctor",
+  },
+  {
+    path: "/services",
+    display: "Services",
+  },
+  {
+    path: "/contact",
+    display: "Contact Us",
+  },
+];
 
 export const Footer = () => {
+  const year = new Date().getFullYear();
   return (
-    <div>Footer</div>
-  )
-}
+    <footer className="pb-16 pt-10">
+      <div className="container">
+        <div className="flex justify-between flex-col md:flex-row flex-wrap gap-[30px]">
+          <div>
+            <img src={logo} alt="" />
+            <p className="text-[16px] leading-7 font-[400] text-textColor mt-4">
+              Copyright &copy; {year} developed by Terence Faid JABO all right
+              reserved.
+            </p>
+            <div className="flex items-center gap-3 mt-4">
+              {socialLinks.map((link, index) => (
+                <Link
+                  to={link.path}
+                  key={index}
+                  className="w-9 h-9 border border-solid border-[#181A1E] rounded-full flex items-center justify-center group hover:bg-primaryColor hover:border-none"
+                >
+                  {link.icon}
+                </Link>
+              ))}
+            </div>
+          </div>
+          <div>
+            <h2 className="text-[20px] leading-[30px] font-[700] mb-6 text-headingColor">
+              Quick Links
+            </h2>
+            <ul>
+              {quickLinks01.map((item,index)=><li key={index}><Link to={item.path} className="text-[16px] leading-7 font-400 text-textColor">{item.display}</Link></li>)}
+            </ul>
+          </div>
+          <div>
+            <h2 className="text-[20px] leading-[30px] font-[700] mb-6 text-headingColor">
+              I want to:
+            </h2>
+            <ul>
+              {quickLinks02.map((item,index)=><li key={index}><Link to={item.path} className="text-[16px] leading-7 font-400 text-textColor">{item.display}</Link></li>)}
+            </ul>
+          </div>
+          <div>
+            <h2 className="text-[20px] leading-[30px] font-[700] mb-6 text-headingColor">
+              Support
+            </h2>
+            <ul>
+              {quickLinks01.map((item,index)=><li key={index}><Link to={item.path} className="text-[16px] leading-7 font-400 text-textColor">{item.display}</Link></li>)}
+            </ul>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
